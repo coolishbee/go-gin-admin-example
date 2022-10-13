2fc364653e69df01198ef73e75caa8ef8ffc19d8
coolishbee <chc3484@gmail.com>
2022-10-13 18:57:59 +0900

    [add] default setting [info] go 1.19, adm 1.2.24

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/signal"
+
+	_ "github.com/GoAdminGroup/go-admin/adapter/gin"              // web framework adapter
+	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/mysql" // sql driver
+	_ "github.com/GoAdminGroup/themes/adminlte"                   // ui theme
+
+	"github.com/GoAdminGroup/go-admin/engine"
+	"github.com/GoAdminGroup/go-admin/template"
+	"github.com/GoAdminGroup/go-admin/template/chartjs"
+	"github.com/gin-gonic/gin"
+
+	"go-gin-admin-example/pages"
+	"go-gin-admin-example/tables"
+)
+
+func main() {
+	startServer()
+}
+
+func startServer() {
+	gin.SetMode(gin.ReleaseMode)
+	gin.DefaultWriter = io.Discard
+
+	r := gin.Default()
+
+	template.AddComp(chartjs.NewChart())
+
+	eng := engine.Default()
+
+	if err := eng.AddConfigFromYAML("./config.yml").
+		AddGenerators(tables.Generators).
+		Use(r); err != nil {
+		panic(err)
+	}
+
+	r.Static("/uploads", "./uploads")
+
+	eng.HTML("GET", "/admin", pages.GetDashBoard)
+	eng.HTMLFile("GET", "/admin/hello", "./html/hello.tmpl", map[string]interface{}{
+		"msg": "Hello world",
+	})
+
+	_ = r.Run(":80")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Print("closing database connection")
+	eng.MysqlConnection().Close()
+}
